691b70480761329e8ef21e61b53ffd1730dd8aa1
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-04-23 21:51:29 +0300

    upd

--- src/dao/productCategory.go
+++ src/dao/productCategory.go
@@ -9,17 +9,23 @@ import (
 type ProductCategoryDAOImpl struct{}
 
 type ProductCategoryDAO interface {
-	GetProductCategory() (model.ProductCategory, error)
+	GetProductCategory(productID int) (model.ProductCategory, error)
 	GetProductsByCategoryID(ID int) ([]model.Product, error)
-	DeleteProductCategory(ProductID int)
+	DeleteProductCategory(productID int) error
 }
 
 func GetProductCategoryDAO() ProductCategoryDAO {
 	return &ProductCategoryDAOImpl{}
 }
 
-func (dao *ProductCategoryDAOImpl) GetProductCategory() (model.ProductCategory, error) {
-	return model.ProductCategory{}, nil
+func (dao *ProductCategoryDAOImpl) GetProductCategory(productID int) (model.ProductCategory, error) {
+	var category model.ProductCategory
+	if err := connection.GetConnection().DB.Table("product_categories").
+		Select("product_categories.id, product_categories.product_id, product_categories.category_id").
+		Where("product_categories.product_id = ?", productID).Scan(&category).Error; err != nil {
+		return emptyProductCategory(), err
+	}
+	return category, nil
 }
 
 func (dao *ProductCategoryDAOImpl) GetProductsByCategoryID(ID int) ([]model.Product, error) {
@@ -38,6 +44,14 @@ func (dao *ProductCategoryDAOImpl) GetProductsByCategoryID(ID int) ([]model.Prod
 	return products, nil
 }
 
-func (dao *ProductCategoryDAOImpl) DeleteProductCategory(ProductID int) {
-	panic("Not implemented")
+func (dao *ProductCategoryDAOImpl) DeleteProductCategory(productID int) error {
+	if err := connection.GetConnection().DB.Where("product_id = ?", productID).Delete(model.ProductCategory{}).Error; err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func emptyProductCategory() model.ProductCategory {
+	return model.ProductCategory{}
 }
