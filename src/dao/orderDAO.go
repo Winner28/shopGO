241b36e53d7d7708d70135ad9b8fb6c48a274258
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-04-27 23:47:28 +0300

    working with orders

--- src/dao/orderDAO.go
+++ src/dao/orderDAO.go
@@ -1,3 +1,26 @@
 package dao
 
-type OrderDAO struct{}
+import (
+	"model"
+	"time"
+)
+
+type OrderDAO interface {
+	createOrder(userID, amount int, date time.Time) (model.Order, error)
+	getOrdersByUserID(userID int)
+}
+
+type OrderDAOIMpl struct {
+}
+
+func GetOrderDAO() OrderDAO {
+	return &OrderDAOIMpl{}
+}
+
+func (dao *OrderDAOIMpl) createOrder(userID, amount int, date time.Time) (model.Order, error) {
+	return model.Order{}, nil
+}
+
+func (dao *OrderDAOIMpl) getOrdersByUserID(userID int) {
+
+}
