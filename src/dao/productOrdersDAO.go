87fdeae79b833d89602fcf040b377b19e8990586
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-05-01 17:23:06 +0300

    Get method in product orders dao impl

--- src/dao/productOrdersDAO.go
+++ src/dao/productOrdersDAO.go
@@ -2,12 +2,14 @@ package dao
 
 import (
 	"connection"
+	"errors"
 	"model"
 )
 
 type (
 	ProductOrderDAO interface {
 		Create(orderID, productID int) error
+		Get(orderID int) (productID int, err error)
 	}
 
 	ProductOrderDAOImpl struct{}
@@ -23,6 +25,24 @@ func (dao *ProductOrderDAOImpl) Create(orderID, productID int) error {
 		return err
 	}
 	return nil
+}
+
+func (dao *ProductOrderDAOImpl) Get(orderID int) (productID int, err error) {
+
+	rows, err := connection.GetConnection().DB.Select("product_id").Where("order_id = ?", orderID).Rows()
+	if err != nil {
+		return -1, err
+	}
+	defer rows.Close()
+	if !rows.Next() {
+		return -1, errors.New("Have no product with such order id!")
+	}
+	rows.Scan(&productID)
+	return productID, nil
+}
+
+func emptyProductOrder() model.ProductOrder {
+	return model.ProductOrder{}
 }
 
 func getProductOrder(orderID, productID int) model.ProductOrder {
